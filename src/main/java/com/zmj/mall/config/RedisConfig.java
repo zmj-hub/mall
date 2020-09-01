51072263bcc3a0adf9ae206e6f73099fdcf8d8da
zmj-hub <1286861559@qq.com>
2020-09-01 17:25:44 +0800

    增加redis

---
+++ src/main/java/com/zmj/mall/config/RedisConfig.java
@@ -0,0 +1,66 @@
+package com.zmj.mall.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import javax.crypto.KeyGenerator;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+@Configuration
+@EnableCaching
+public class RedisConfig{
+
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory){
+
+        RedisTemplate<Object, Object> template=new RedisTemplate<Object, Object>();
+
+        template.setConnectionFactory(connectionFactory);
+        //实现序列化和反序列化redis的key值
+        template.setKeySerializer(new StringRedisSerializer());
+        //实现序列化和反序列化redis的value值,默认使用JdkSerializationRedisSerializer
+        //template.setValueSerializer(new RedisObjectSerializer());
+        //template.setValueSerializer();
+        return template;
+
+    }
+
+
+//    @Bean
+//    public CacheManager cacheManager(RedisTemplate<Object, Object> redisTemplate) {
+//        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+//        // cacheManager.setCacheNames(Arrays.asList("users", "emptyUsers"));
+//        cacheManager.setUsePrefix(true);
+//        // Number of seconds before expiration. Defaults to unlimited (0)
+//        cacheManager.setDefaultExpiration(1800L);
+//        return cacheManager;
+//    }
+
+    //自定义keyGenerator必须实现org.springframework.cache.interceptor.KeyGenerator接口
+//    @Bean
+//    public KeyGenerator accountKeyGenerator() {
+//        return new KeyGenerator(){
+//            @Override
+//            public Object generate(Object target, Method method, Object... params) {
+//                //first parameter is caching object
+//                //second paramter is the name of the method, we like the caching key has nothing to do with method name
+//                //third parameter is the list of parameters in the method being called
+//                return target.getClass().toString() + "accountId:" + params[0].toString();
+//            }
+//        };
+//    }
+
+}
