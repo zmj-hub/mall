b4864b03d63091e614cd9f91065225f4074cabc3
zmj-hub <1286861559@qq.com>
2020-09-11 17:53:59 +0800

    集成nacos

--- src/main/java/com/zmj/mall/MallApplication.java
+++ src/main/java/com/zmj/mall/MallApplication.java
@@ -1,8 +1,12 @@
 package com.zmj.mall;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@NacosPropertySource(dataId ="mall", autoRefreshed = true)
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MallApplication {
 
