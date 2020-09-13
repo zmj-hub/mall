1f57cfc069e334eccf11b936586d5dbb528a10f1
zmj <1286861559@qq.com>
2020-09-13 22:13:00 +0800

    NACOS

--- src/main/java/com/zmj/mall/MallApplication.java
+++ src/main/java/com/zmj/mall/MallApplication.java
@@ -1,9 +1,15 @@
 package com.zmj.mall;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @NacosPropertySource(dataId ="mall", autoRefreshed = true)
 @EnableDiscoveryClient
@@ -14,4 +20,12 @@ public class MallApplication {
         SpringApplication.run(MallApplication.class, args);
     }
 
+    @NacosValue(value = "${zmj}", autoRefreshed = true)
+    private String serverName;
+
+    @RequestMapping(value = "/test", method = GET)
+    @ResponseBody
+    public String get() {
+        return serverName;
+    }
 }
